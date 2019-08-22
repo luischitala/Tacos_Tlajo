602f1e5fc7993ae0179a1766938e1be7fc2607cf
Luis Chitala <lrchitala@gmail.com>
2019-08-22 17:04:58 -0500

    Menu and tables done, left menu has to be completed

--- views/home.view.php
+++ views/home.view.php
@@ -3,17 +3,209 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat|Roboto&display=swap" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="../css/estilos.css">
+	<link rel="stylesheet" type="text/css" href="../css/fontello.css">
 	<link rel="stylesheet" href="../css/bootstrap.min.css">
-	<title>Plantilla</title>
+	<title>Bienvenido</title>
 </head>
 <body>
-	<div class="container">
-		<div class="row">
-			<div class="col">
-			
+	<div class="container-fluid">
+		<main class="main col">
+			<div class="row">
+				<div class="col-6 mapa">
+					<div class="row">
+						<div class="col-12 mesas">
+							
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-12 menum-fondo">
+							<div class="menu-mapa">
+								<div class="contenedor d-flex flex-wrap">
+									<div class="caja">
+
+									</div>
+									<div class="caja">
+										
+									</div>
+									<div class="caja">
+										
+									</div>
+									<div class="caja">
+										
+									</div>
+									<div class="caja">
+									</div>
+								</div>
+							</div>
+						</div>
+
+					</div>
+				</div>
+				<div class="col-6 menup-fondo">
+					<div class="menu1">
+						<div class="contenedor d-flex flex-wrap ">
+									<div class="caja ">
+										<a href="#"><i class="icon-doc-add"></i><span>Nueva Cuenta</span></a>	
+									</div>
+									<div class="caja">
+										<a href="#"><i class="icon-right-hand"></i><span>Captura</span></a>	
+									</div>
+									<div class="caja">
+										<a href="#"><i class="icon-fast-food"></i><span>Venta Rápida</span></a>	
+									</div>
+									<div class="caja">
+										<a href="#"><i class="icon-home"></i><span>Venta Domicilio</span></a>	
+									</div>
+									<div class="caja">
+										<a href="#"><i class="icon-money"></i><span>Cobro</span></a>	
+									</div>
+						</div>
+					</div>
+				<div class="row">
+					<div class="col-5 detalles">
+							<div class="entrada d-flex flex-wrap justify-content-between">
+							<h3>Cuenta</h3><input type="text" name="">
+							<h3>Mesa</h3><input type="text" name="">
+							<h3>Personas</h3><input type="text" name="">
+							<h3>Mesero</h3><input type="text" name="">
+							</div>
+					</div>
+					<div class="col-5 detalles">
+							<div class="entrada d-flex flex-wrap justify-content-between">
+							<h3>Reserva</h3><input type="text" name="">
+							<h3>Apertura</h3><input type="text" name="">
+							<h3>Cierre</h3><input type="text" name="">
+							<h3>Folio</h3><input type="text" name="">
+							</div>
+					</div>
+				</div>	
+					<div class="col-12 d-flex flex-wrap tabla" >
+					 	<table id="tablaprincipal">
+					 		 <tr>
+							   <th>Mov</th>
+							   <th>Comanda</th> 
+							   <th>Cant</th>
+							   <th>Clave</th>
+							   <th>Descripción</th> 
+							   <th>Desc</th>
+							   <th>Precio</th>
+							   <th>IVA</th>
+							   <th>Importe Total</th>
+							 </tr>
+							 <tr>
+							    <td></td>
+							    <td></td> 
+							    <td></td>
+							    <td></td>
+							    <td></td>
+							    <td></td>
+							    <td></td>
+							    <td></td>
+							    <td></td>
+							 </tr>
+							 <tr>
+							    <td></td>
+							    <td></td> 
+							    <td></td>
+							    <td></td>
+							    <td></td>
+							    <td></td>
+							    <td></td>
+							    <td></td>
+							    <td></td>
+							 </tr>
+							  <tr>
+							    <td></td>
+							    <td></td> 
+							    <td></td>
+							    <td></td>
+							    <td></td>
+							    <td></td>
+							    <td></td>
+							    <td></td>
+							    <td></td>
+							 </tr>
+							  <tr>
+							    <td></td>
+							    <td></td> 
+							    <td></td>
+							    <td></td>
+							    <td></td>
+							    <td></td>
+							    <td></td>
+							    <td></td>
+							    <td></td>
+							 </tr>
+							  <tr>
+							    <td></td>
+							    <td></td> 
+							    <td></td>
+							    <td></td>
+							    <td></td>
+							    <td></td>
+							    <td></td>
+							    <td></td>
+							    <td></td>
+							 </tr>
+							  <tr>
+							    <td></td>
+							    <td></td> 
+							    <td></td>
+							    <td></td>
+							    <td></td>
+							    <td></td>
+							    <td></td>
+							    <td></td>
+							    <td></td>
+							 </tr>
+							  <tr>
+							    <td></td>
+							    <td></td> 
+							    <td></td>
+							    <td></td>
+							    <td></td>
+							    <td></td>
+							    <td></td>
+							    <td></td>
+							    <td></td>
+							 </tr>
+							  <tr>
+							    <td></td>
+							    <td></td> 
+							    <td></td>
+							    <td></td>
+							    <td></td>
+							    <td></td>
+							    <td></td>
+							    <td></td>
+							    <td></td>
+							 </tr>
+					 	</table>
+					</div>
+					<div class="row">
+						<div class="col-6 d-flex justify-content-center">
+							<div class="form-group shadow-textarea observaciones">
+ 							 	<label for="exampleFormControlTextarea6">Observaciones</label>
+  								<textarea class="form-control z-depth-1" id="exampleFormControlTextarea6" rows="3" placeholder="Write something here..."></textarea>
+							</div>
+						</div>
+					<div class="col-5 detalles">
+							<div class="entrada d-flex flex-wrap justify-content-between">
+							<h3>Reserva</h3><input type="text" name="">
+							<h3>Apertura</h3><input type="text" name="">
+							<h3>Cierre</h3><input type="text" name="">
+							<h3>Folio</h3><input type="text" name="">
+							</div>
+					</div>
+					</div>
+				</div>
+				
 			</div>
-		</div>	
+		</main>	
 	</div>
+	
 	
 	<script src="../js/jquery-3.4.1.min.js"></script>
 	<script src="../js/bootstrap.min.js"></script>
